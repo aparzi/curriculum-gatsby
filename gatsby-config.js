60c5dcae16a83cc8b3cd8bd383e51fce1610e0db
aparzi <angeloparziale94@gmail.com>
2019-11-19 00:03:50 +0100

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `angeloparziale.it`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    siteUrl: `https://aparzialecv.netlify.com/it`,
+    siteUrl: `https://aparzialecv.netlify.com`,
     author: `Aparziale`,
     menuLinks: [
       {
